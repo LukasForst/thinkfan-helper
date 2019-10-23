30cc883dcbbd05d40305223553adc82de0d2b31a
Lukas Forst <lukas.forst@blindspot.ai>
2019-10-23 10:29:25 +0200

    update readme and spacing

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-This utility solves problems on some kernels when `hwmon` resources are not in the fixed path and they can be changed during the reboot.
-For that reason, it is sometimes not possible to correctly configure [thinkfan](https://github.com/vmatare/thinkfan) since it requires specifying the correct folders where the `hwmon` is located.
+## About
 
-This script should be executed after the boot, when the `hwmon` is set and it scans the folder `/sys/devices/platform/coretemp.0/hwmon` for `hwmon` folder and then update thinkfan configuration located at `/etc/thinkfan.conf` accordingly.
+This utility solves problems on some kernels when `hwmon` resources are not in the fixed path 
+and they can be changed during the reboot.
+For that reason, it is sometimes not possible to correctly configure [thinkfan](https://github.com/vmatare/thinkfan)
+ since it requires specifying the correct folders where the `hwmon` is located.
+
+This script should be executed after the boot, when the `hwmon` is set and it scans the folder
+ `/sys/devices/platform/coretemp.0/hwmon` for `hwmon` folder and then update thinkfan configuration
+  located at `/etc/thinkfan.conf` accordingly.
 If the configuration changed, script restarts the thinfan service.
+
+## Predisposition
+
+* Python (obviously) > 3.6
+* Thinkfan config located in `/etc/thinkfan.conf`
+    * also, the script has to have rights to edit the file -> therefore run it as root OR as a user that can write 
+    to the file (this is preferred)
--- thinkfan-helper.py
+++ thinkfan-helper.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-
 import os
 import re
 import subprocess
-import sys
 from functools import cmp_to_key
 
+
 def open_settings(path: str):
-    with open(path, "r", encoding="UTF-8") as file: 
+    with open(path, "r", encoding="UTF-8") as file:
         return file.readlines()
 
 
@@ -16,53 +15,59 @@ def get_hwmon_location():
     hwmon_dirs = os.listdir(base_path)
     assert len(hwmon_dirs) == 1
     hwmon_dir = f"{base_path}/{hwmon_dirs[0]}"
-    return [f"hwmon {hwmon_dir}/{x}\n" for x in os.listdir(hwmon_dir) if os.path.isfile(f"{hwmon_dir}/{x}") and re.match("temp[0-9]+_input", x)]
+    return [f"hwmon {hwmon_dir}/{x}\n" for x in os.listdir(hwmon_dir) if
+            os.path.isfile(f"{hwmon_dir}/{x}") and re.match("temp[0-9]+_input", x)]
 
 
 def update_settings(current, new):
-    config = [x for x in current if(not x.startswith("hwmon"))] + new
+    config = [x for x in current if (not x.startswith("hwmon"))] + new
     return sorted(config, key=cmp_to_key(lambda i1, i2: line_fitness(i1) - line_fitness(i2)))
+
 
 def line_fitness(line: str):
     if line.startswith("tp_fan"):
         return 1
     elif line.startswith("hwmon"):
         return 3
-    elif(line.startswith("(")):
+    elif line.startswith("("):
         return 5
     else:
         return 4
+
 
 def are_same(current, new):
     cur = set([x.strip() for x in current if x.startswith("hwmon")])
     ne = set([x.strip() for x in new if x.startswith("hwmon")])
     return cur.issubset(ne) and cur.issuperset(ne)
 
+
 def save_settings(current, new):
     updated_config = "".join(update_settings(current, new))
     print(f"Updating config!\n{updated_config}")
     write_settings(config_path, updated_config)
 
+
 def write_settings(path: str, payload: str):
     with open(path, "w", encoding="UTF-8") as file:
         file.write(payload)
 
+
 def restart_thinkfan():
     process = subprocess.Popen("systemctl restart thinkfan.service".split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
-    
-    if(error):
+
+    if error:
         print(f"Error ocurred during the thinkfan restart!\n{error}")
     else:
         print("Service was restarted successfully, everything set.")
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     config_path = "/etc/thinkfan.conf"
     current = open_settings(config_path)
     new = get_hwmon_location()
 
-    if(are_same(current, new)):
+    if are_same(current, new):
         print("All set! No change detected.")
     else:
         save_settings(current, new)
